0c8a01226a391423b5af3d8d209dae82ed974ea3
Scheyk <kathrin.dostal@gmail.com>
2020-10-29 09:39:38 +0100

    Projektdateien hinzufügen.

---
+++ Csharp-Day3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Csharp_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cornel();
+            Samira();
+            Kathi();
+        }
+
+        static void Cornel()
+        {
+            var tasks = new Cornel.Cornel();
+           
+        }
+
+        static void Samira()
+        {
+            var tasks = new Samira.Samira();
+        }
+
+        static void Kathi()
+        {
+            var tasks = new Kathi.Kathi();
+        }
+    }
+}
